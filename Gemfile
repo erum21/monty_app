17784467a8ab8f07adef55dd10df4a685168c4f0
Edwin Vega <edwin.ponc@gmail.com>
2012-06-27 13:49:19 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '3.2.5'
+  gem 'coffee-rails', '3.2.2'
+
+  gem 'uglifier', '1.2.5'
+end
+
+gem 'jquery-rails', '2.0.2'
+
+group :production do
+  gem 'pg', '0.14.0'
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MontyApp::Application.config.secret_token = 'f041531f34704bfb72689040f42ba9cc1d881ab88d0b73cc7ca5796d610f33309926999a831b761ce5748304fd015a51d3aacbac1b50d1ee475781dfaac5ce4b'
